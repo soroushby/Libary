ba75709e79a20f563c74eff32e25cb0bb5b75fbe
Soroushby <sorosh.bayanati@yahoo.com>
2022-10-14 13:49:31 +0330

    worked on contact us form and the table

--- src/app/books/books-card-list/books-card-list.component.ts
+++ src/app/books/books-card-list/books-card-list.component.ts
@@ -1,5 +1,7 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
@@ -29,12 +31,26 @@ export class BooksCardListComponent implements AfterViewInit {
   private _books: BookUnDetailed[] = [];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
+  constructor(private _liveAnnouncer: LiveAnnouncer) {
     this.dataSource = new MatTableDataSource(this.books);
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
